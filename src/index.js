001a5d27dbf186a04416be28a70f5f63692d1097
Christian Hettlage <hettlage@saao.ac.za>
2017-08-02 09:23:05 +0200

    Show total time distribution with dummy data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>Hello TAC Pages!</h1>,
+import TotalTimeDistribution from './components/total-time-distribution';
+import { partnerByCode } from './util/partners';
+
+const AMNH = partnerByCode('AMNH');
+const RSA = partnerByCode('RSA');
+
+const proposals = [
+    {
+        timeRequests: [
+            {
+                partner: AMNH,
+                time: 3.7
+            },
+            {
+                partner: RSA,
+                time: 5
+            }
+        ]
+    },
+    {
+        timeRequests: [
+            {
+                partner: AMNH,
+                time: 54.57
+            },
+        ]
+    },
+    {
+        timeRequests: [
+            {
+                partner: RSA,
+                time: 17
+            }
+        ]
+    },
+];
+
+render(<svg width={1000} height={1000}>
+    <TotalTimeDistribution proposals={proposals} width={500} height={500} partner={AMNH}/>
+        </svg>,
         document.getElementById('react-content'));
