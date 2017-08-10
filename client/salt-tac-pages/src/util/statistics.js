8db14c30710d5eaffa7354c13afa45e5c340c6d3
Christian Hettlage <hettlage@saao.ac.za>
2017-08-10 14:56:31 +0200

    Added statistics functions and unit tests

--- client/salt-tac-pages/src/util/statistics.js
+++ client/salt-tac-pages/src/util/statistics.js
@@ -1,26 +1,206 @@
+import * as _ from 'lodash';
+
 // Utility functions for statistics
 
 /**
- * The list of non-zero requested hours for a partner.
+ * Compare two semesters. The semesters must be strings of the form 'yyyy-s', where yyyy is the four digit year and s is
+ * either 1 or 2.
+ *
+ * -1, 0 or 1 is returned, depending on whether the first semester is earlier than, the same as or later than the
+ * second semester.
+ *
+ * Parameters:
+ *
+ * semester1: string
+ *     The first semester.
+ * semester2: string
+ *     The second semester.
+ */
+export function compareSemesters(semester1, semester2) {
+    const [year1, sem1] = semester1.split('-');
+    const [year2, sem2] = semester2.split('-');
+
+    if (year1 < year2) {
+        return -1;
+    } else if (year1 === year2) {
+        if (sem1 < sem2) {
+            return -1;
+        } else if (sem1 === sem2) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } else {
+        return 1;
+    }
+}
+
+export function proposalsCount(proposals) {
+    return proposals.length;
+}
+
+export function newProposalsCount(proposals, semester) {
+    return proposals.filter(proposal => compareSemesters(proposal.semester, semester) === 0).length;
+}
+
+export function resubmittedProposalsCount(proposals, semester) {
+    return proposals.filter(proposal => compareSemesters(proposal.semester, semester) < 0).length;
+}
+
+export function isLongtermProposal(proposal) {
+    const allSemesters = proposal.timeRequests.map(request => request.semester);
+    const uniqueSemesters = new Set(allSemesters);
+
+    return uniqueSemesters.size > 1;
+}
+
+export function longtermProposalsCount(proposals) {
+    return proposals.filter(isLongtermProposal).length;
+}
+
+export function newLongtermProposalsCount(proposals, semester) {
+    return proposals.filter(isLongtermProposal)
+            .filter(proposal => compareSemesters(proposal.semester, semester) === 0)
+            .length;
+}
+
+/**
+ * The time which can be spent on observations. This is summed over priorities 0 to 3. It may differ from the time which
+ * can be allocated to proposals. The latter is calculated by the function availableAllocationTime.
+ *
+ * Parameters:
+ *
+ * availableTime: object
+ *     The available time. This must be for the given partner and semester. The object must contain the key
+ *     'scienceTime', and the corresponding object must contain the keys 'p0and1', 'p2' and 'p3'.
+ */
+export function availableScienceTime(availableTime) {
+    return availableTime.scienceTime.p0and1
+            + availableTime.scienceTime.p2
+            + availableTime.scienceTime.p3;
+}
+
+/**
+ * The time which can be allocated to proposals. This is summed over priorities 0 to 3. It may differ from the time
+ * which can be observed. The latter is calculated by the function availableScienceTime.
+ *
+ * Parameters:
+ *
+ * availableTime: object
+ *     The available time. This must be for the given partner and semester. The object must contain the key
+ *     'allocationTime', and the corresponding object must contain the keys 'p0and1', 'p2' and 'p3'.
+ */
+export function availableAllocationTime(availableTime) {
+    return availableTime.allocationTime.p0and1
+            + availableTime.allocationTime.p2
+            + availableTime.allocationTime.p3;
+}
+
+/**
+ * The list of non-zero requested times (in hours) for a partner and semester.
  *
  * Parameters:
  *
  * proposals: array
  *     Proposals.
  * partner: Partner
- *     Partner
+ *     Partner.
+ * semester: string
+ *     Semester.
  */
-export function requestedHours(proposals, partner) {
+export function requestedTimes(proposals, partner, semester) {
     const timeRequest = (proposal, partner) => proposal.timeRequests.find(t => t.partner.code === partner.code);
 
     return proposals
-            .filter(proposal => partner.requestsTimeFor(proposal))
+            .filter(proposal => !proposal.isP4)
+            .filter(proposal => partner.hasTimeRequestFor(proposal, semester))
             .reduce(
                     (a, proposal) => {
-                        const req = timeRequest(proposal, partner);
-                        return req ? [...a, req.time] : a
+                        const req = timeRequest(proposal, partner, semester);
+                        return [...a, req.time]
                     },
                     []
             )
             .map(t => t / 3600);
+}
+
+/**
+ * The total requested time for a partner and semester, summed over all proposals.
+ *
+ * Parameters:
+ *
+ * proposals: array
+ *     Proposals.
+ * partner: Partner
+ *     Partner.
+ * semester: string
+ *     Semester.
+ */
+export function totalRequestedTime(proposals, partner, semester) {
+    return _.sum(requestedTimes(proposals, partner, semester));
+}
+
+/**
+ * The oversubscription rate. This is the ratio of the total requested time and the available science time.
+ *
+ * Parameters:
+ *
+ * proposals: array
+ *     Proposals.
+ * partner: Partner
+ *     Partner.
+ * semester: string
+ *     Semester.
+ * availableTime: object
+ *     The available time. This must be for the given partner and semester. The object must contain the key
+ *     'scienceTime', and the corresponding object must contain the keys 'p0and1', 'p2' and 'p3'.
+ */
+export function oversubscriptionRate(proposals, partner, semester, availableTime) {
+    return totalRequestedTime(proposals, partner, semester) / availableScienceTime(availableTime)
+}
+
+/**
+ * The average time (in hours) requested per proposal for a partner and semester. This is the ratio of the total
+ * requested time and the number of proposals.
+ */
+export function averageRequestedTime(proposals, partner, semester) {
+    return totalRequestedTime(proposals, partner, semester) / proposalsCount(proposals);
+}
+
+export function thesisProjectsCount(proposals) {
+    return proposals.reduce((count, proposal) => count + proposal.thesisProjects.length, 0);
+}
+
+export function p4ProposalsCount(proposals) {
+    return proposals.filter(proposal => proposal.isP4).length;
+}
+
+/**
+ * The sublist of proposals whose maximum seeing is greater than or equal to a given seeing.
+ *
+ * Parameters:
+ *
+ * proposals: array
+ *     Proposals.
+ * seeing: number
+ *     Seeing.
+ */
+export function proposalsForSeeing(proposals, seeing) {
+    return proposals.filter(proposal => proposal.maxSeeing >= seeing);
+}
+
+export function proposalsForTransparency(proposals, transparency) {
+    return proposals.filter(proposal => proposal.transparency === transparency);
+}
+
+export function proposalsWithInstrumentConfig(proposals, instrumentConfig) {
+    return proposals.filter(
+            proposal => proposal.instrumentConfigurations.some(
+                    // are all properties of instrumentConfig existing and the same in configuration c?
+                    c => Object.keys(instrumentConfig).reduce(
+                            (allFound, key) => allFound && key in c && c[key] === instrumentConfig[key],
+                            true
+                    )
+            )
+    );
 }
--- client/salt-tac-pages/src/util/statistics.test.js
+++ client/salt-tac-pages/src/util/statistics.test.js
@@ -1,3 +1,7 @@
+import {
+    HrsMode, Instrument, RssDetectorMode, RssObservingMode, SalticamDetectorMode,
+    Transparency
+} from '../enumerations';
 import * as statistics from './statistics';
 import Partner from './partner';
 
@@ -6,43 +10,340 @@ const RSA = Partner.partnerByCode('RSA');
 const UW = Partner.partnerByCode('UW');
 
 function defaultProposals() {
-    const timeRequests = {
-        AMNH: [12.6, 22.0, 1.9, 25., 58.9, 31, 29.5, 7.8],
-        RSA: [0, 0, 0, 78., 0, 99., 0, 0]
-    };
+    return [
+        {
+            proposalCode: '2017-1-MLT-001',
+            semester: '2017-1',
+            timeRequests: [
+                { partner: AMNH, semester: '2017-1', time: 4.5 * 3600 },
+                { partner: AMNH, semester: '2017-2', time: 7.9 * 3600 },
+                { partner: RSA, semester: '2017-1', time: 10.9 * 3600 }
+            ],
+            isP4: false,
+            thesisProjects: [],
+            maxSeeing: 3.2,
+            transparency: Transparency.CLEAR,
+            instrumentConfigurations: [
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.NORMAL,
+                    observingMode: RssObservingMode.FABRY_PEROT
+                },
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.NORMAL,
+                    observingMode: RssObservingMode.SPECTROSCOPY
+                }
+            ]
+        },
 
-    const proposals = timeRequests.AMNH.map((_, i) => (
-            {
-                timeRequests: [
-                    {
-                        partner: AMNH,
-                        time: 3600 * timeRequests.AMNH[i]
-                    },
-                    {
-                        partner: RSA,
-                        time: 3600 * timeRequests.RSA[i]
-                    }
-                ]
-            }
-    ));
-    proposals[2].timeRequests.push({
-                                       partner: UW,
-                                       time: 23.9 * 3600
-                                   });
-    return proposals;
+        {
+            proposalCode: '2017-2-SCI-001',
+            semester: '2017-2',
+            timeRequests: [
+                { partner: RSA, semester: '2017-2', time: 32 * 3600 }
+            ],
+            isP4: false,
+            thesisProjects: [
+                {},
+                {}
+            ],
+            maxSeeing: 2.1,
+            transparency: Transparency.THICK_CLOUD,
+            instrumentConfigurations: [
+                {
+                    instrument: Instrument.SALTICAM,
+                    detectorMode: SalticamDetectorMode.DRIFT_SCAN
+                },
+                {
+                    instrument: Instrument.HRS,
+                    hrsMode: HrsMode.MEDIUM_RESOLUTION
+                }
+            ]
+        },
+
+        {
+            proposalCode: '2017-2-MLT-001',
+            semester: '2017-2',
+            timeRequests: [
+                { partner: AMNH, semester: '2017-2', time: 11.1 * 3600 },
+                { partner: UW, semester: '2018-1', time: 14.7 * 3600 }
+            ],
+            isP4: false,
+            thesisProjects: [
+                {},
+                {},
+                {}
+            ],
+            maxSeeing: 2.3,
+            transparency: Transparency.ANY,
+            instrumentConfigurations: [
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.DRIFT_SCAN,
+                    observingMode: RssObservingMode.FABRY_PEROT
+                }
+            ]
+        },
+
+        {
+            proposalCode: '2017-2-MLT-002',
+            semester: '2017-2',
+            timeRequests: [
+                { partner: RSA, semester: '2017-2', time: 78.1 * 3600 },
+                { partner: RSA, semester: '2018-1', time: 68.4 * 3600 },
+                { partner: RSA, semester: '2018-2', time: 54.4 * 3600 },
+            ],
+            isP4: true,
+            thesisProjects: [],
+            maxSeeing: 1.7,
+            transparency: Transparency.CLEAR,
+            instrumentConfigurations: [
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.NORMAL,
+                    observingMode: RssObservingMode.IMAGING
+                }
+            ]
+        }
+    ];
 }
 
-describe('requestedHours', () => {
-    it('should return the list of requested hours', () => {
-        const proposals = defaultProposals();
+function defaultAvailableTimes() {
+    return {
+        scienceTime: {
+            'p0and1': 45.9,
+            'p2': 123.7,
+            'p3': 201.8
+        },
+        allocationTime: {
+            'p0and1': 56.0,
+            'p2': 187.8,
+            'p3': 194.5
+        }
+    };
+}
 
-        expect(statistics.requestedHours(proposals, AMNH))
-                .toEqual([12.6, 22.0, 1.9, 25., 58.9, 31, 29.5, 7.8]);
+describe('compareSemesters', () => {
+    it('should compare two semesters', () => {
+        expect(statistics.compareSemesters('2016-1', '2017-1')).toBe(-1);
+        expect(statistics.compareSemesters('2016-2', '2017-1')).toBe(-1);
+        expect(statistics.compareSemesters('2017-1', '2017-1')).toBe(0);
+        expect(statistics.compareSemesters('2017-2', '2017-1')).toBe(1);
+        expect(statistics.compareSemesters('2018-1', '2017-1')).toBe(1);
+        expect(statistics.compareSemesters('2018-2', '2017-1')).toBe(1);
 
-        expect(statistics.requestedHours(proposals, RSA))
-                .toEqual([78., 99.]);
+        expect(statistics.compareSemesters('2016-1', '2017-2')).toBe(-1);
+        expect(statistics.compareSemesters('2016-2', '2017-2')).toBe(-1);
+        expect(statistics.compareSemesters('2017-1', '2017-2')).toBe(-1);
+        expect(statistics.compareSemesters('2017-2', '2017-2')).toBe(0);
+        expect(statistics.compareSemesters('2018-1', '2017-1')).toBe(1);
+        expect(statistics.compareSemesters('2018-2', '2017-1')).toBe(1);
+    })
+});
 
-        expect(statistics.requestedHours(proposals, UW))
-                .toEqual([23.9]);
+describe('proposalsCount', () => {
+   it('should calculate the number of proposals', () => {
+       expect(statistics.proposalsCount(defaultProposals())).toBe(4);
+   });
+});
+
+describe('newProposalsCount', () => {
+    it('should calculate the number of new proposals', () => {
+        expect(statistics.newProposalsCount(defaultProposals(), '2017-1')).toBe(1);
+        expect(statistics.newProposalsCount(defaultProposals(), '2017-2')).toBe(3);
     });
 });
+
+describe('resubmittedProposalsCount', () => {
+    it('should calculate the number of resubmitted proposals', () => {
+        expect(statistics.resubmittedProposalsCount(defaultProposals(), '2017-2')).toBe(1);
+    });
+});
+
+describe('longtermProposalsCount', () => {
+    it('should calculate the number of longterm proposals', () => {
+        expect(statistics.longtermProposalsCount(defaultProposals())).toBe(3);
+    });
+});
+
+describe('newLongtermProposalsCount', () => {
+    it('should calculate the number of new longterm proposals', () => {
+        expect(statistics.newLongtermProposalsCount(defaultProposals(), '2017-2')).toBe(2);
+    });
+});
+
+describe('availableScienceTime', () => {
+    it('should calculate the available science time', () => {
+        expect(statistics.availableScienceTime(defaultAvailableTimes())).toBe(45.9 + 123.7 + 201.8);
+    });
+});
+
+describe('availableAllocationTime', () => {
+    it('should calculate the available allocation time', () => {
+        expect(statistics.availableAllocationTime(defaultAvailableTimes())).toBe(56.0 + 187.8 + 194.5);
+    });
+});
+
+describe('requestedTimes', () => {
+    it('should give the list of requested times', () => {
+        expect(statistics.requestedTimes(defaultProposals(), RSA, '2017-2')).toEqual([32]);
+    });
+
+    it('should include time requests for zero time', () => {
+        const proposals = [
+            {
+                timeRequests: [
+                    { partner: AMNH, semester: '2017-2', time: 56 * 3600 },
+                ],
+                isP4: false
+            },
+            {
+                timeRequests: [
+                    { partner: AMNH, semester: '2017-2', time: 0 },
+                ],
+                isP4: false
+            },
+            {
+                timeRequests: [
+                    { partner: AMNH, semester: '2017-2', time: 36 * 3600 },
+                ],
+                isP4: false
+            }
+        ];
+
+        expect(statistics.requestedTimes(proposals, AMNH, '2017-2')).toEqual([56, 0, 36]);
+    });
+});
+
+describe('totalRequestedTime', () => {
+    it('should calculate the total requested time', () => {
+        expect(statistics.totalRequestedTime(defaultProposals(), RSA, '2017-2')).toBe(32);
+    });
+});
+
+describe('oversubscriptionRate', () => {
+    it('should calculate the oversubscription rate', () => {
+        expect(statistics.oversubscriptionRate(defaultProposals(), RSA, '2017-2', defaultAvailableTimes()))
+                .toBe((32) / (45.9 + 123.7 + 201.8));
+    });
+});
+
+describe('averageRequestedTime', () => {
+    it('should calculate the average requested time', () => {
+        expect(statistics.averageRequestedTime(defaultProposals(), RSA, '2017-2'), defaultAvailableTimes())
+                .toBe((32) / 4);
+    });
+});
+
+describe('thesisProjectCount', () => {
+    it('should give the number of thesis projects associated with the proposals', () => {
+        expect(statistics.thesisProjectsCount(defaultProposals())).toBe(5);
+    });
+});
+
+describe('p4ProposalsCount', () => {
+    it('should give the number of P4 proposals', () => {
+        expect(statistics.p4ProposalsCount(defaultProposals())).toBe(1);
+    });
+});
+
+describe('proposalsForSeeing', () => {
+    it('should give the list of proposals for a seeing', () => {
+        const proposalCodesForSeeing = seeing =>
+                statistics.proposalsForSeeing(defaultProposals(), seeing).map(proposal => proposal.proposalCode);
+
+        expect(proposalCodesForSeeing(6))
+                .toEqual([]);
+        expect(proposalCodesForSeeing(3.2))
+                .toEqual(['2017-1-MLT-001']);
+        expect(proposalCodesForSeeing(3.1))
+                .toEqual(['2017-1-MLT-001']);
+        expect(proposalCodesForSeeing(2.3))
+                .toEqual(['2017-1-MLT-001', '2017-2-MLT-001']);
+        expect(proposalCodesForSeeing(2.11))
+                .toEqual(['2017-1-MLT-001', '2017-2-MLT-001']);
+        expect(proposalCodesForSeeing(2.1))
+                .toEqual(['2017-1-MLT-001', '2017-2-SCI-001', '2017-2-MLT-001']);
+        expect(proposalCodesForSeeing(1.8))
+                .toEqual(['2017-1-MLT-001', '2017-2-SCI-001', '2017-2-MLT-001']);
+        expect(proposalCodesForSeeing(1.7))
+                .toEqual(['2017-1-MLT-001', '2017-2-SCI-001', '2017-2-MLT-001', '2017-2-MLT-002']);
+    });
+});
+
+describe('proposalsForTransparency', () => {
+    it('should give the list of proposals for a transparency', () => {
+        const proposalCodesForTransparency = transparency =>
+                statistics.proposalsForTransparency(defaultProposals(), transparency)
+                        . map(proposal => proposal.proposalCode);
+
+        expect(proposalCodesForTransparency(Transparency.THICK_CLOUD))
+                .toEqual(['2017-2-SCI-001']);
+
+        expect(proposalCodesForTransparency(Transparency.CLEAR))
+                .toEqual(['2017-1-MLT-001', '2017-2-MLT-002']);
+
+        expect(proposalCodesForTransparency(Transparency.THIN_CLOUD))
+                .toEqual([]);
+    });
+});
+
+describe('proposalsWithInstrumentConfig', () => {
+    it('should give the list of proposals with an instrument configuration', () => {
+        const proposalCodesWithInstrumentConfig = instrumentConfiguration =>
+                statistics.proposalsWithInstrumentConfig(defaultProposals(), instrumentConfiguration)
+                        .map(proposal => proposal.proposalCode);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.SALTICAM,
+                    detectorMode: SalticamDetectorMode.DRIFT_SCAN
+                }
+        )).toEqual(['2017-2-SCI-001']);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.SALTICAM,
+                    detectorMode: SalticamDetectorMode.NORMAL
+                }
+        )).toEqual([]);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.NORMAL
+                }
+        )).toEqual(['2017-1-MLT-001', '2017-2-MLT-002']);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.DRIFT_SCAN
+                }
+        )).toEqual(['2017-2-MLT-001']);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.RSS,
+                    observingMode: RssObservingMode.FABRY_PEROT
+                }
+        )).toEqual(['2017-1-MLT-001', '2017-2-MLT-001']);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.RSS,
+                    observingMode: RssObservingMode.SPECTROSCOPY
+                }
+        )).toEqual(['2017-1-MLT-001']);
+
+        expect(proposalCodesWithInstrumentConfig(
+                {
+                    instrument: Instrument.RSS,
+                    detectorMode: RssDetectorMode.NORMAL,
+                    observingMode: RssObservingMode.FABRY_PEROT
+                }
+        )).toEqual(['2017-1-MLT-001']);
+    });
+});
+
