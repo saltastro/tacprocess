9d8d644904d97d53b1447e018fa90d600bfdd1cd
Christian Hettlage <hettlage@saao.ac.za>
2017-08-11 23:49:27 +0200

    Changes to the login functionality

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from functools import wraps
 
-from flask import Flask, g, request
+from flask import Flask, g, jsonify, request
+from flask_cors import CORS
 from flask_graphql import GraphQLView
 from server.schema import schema
+import json
 import os
 
 from itsdangerous import TimedJSONWebSignatureSerializer as JWT
@@ -11,6 +13,7 @@ from flask_httpauth import HTTPTokenAuth
 is_developoment = 'FLASK_DEBUG' in os.environ and os.environ['FLASK_DEBUG'] == '1'
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'top secret!'
 app.debug = True
 
@@ -21,14 +24,17 @@ auth = HTTPTokenAuth(scheme='Token')
 
 
 @app.route("/login", methods=['POST'])
-def get_token():
+def login():
     from server.main import User
-    us = {"username": 'nhlavutelo',
-          "password": os.environ["PASSWORD"]}
-    user = User(user_cred=us)  # Todo send a json(username and password) from the web not this
-    print(user.get_token())
-    return user.get_token()
-    
+    user = User(request.json)
+    return jsonify({
+        'token': user.get_token().decode('utf-8'),
+        'username': user.username,
+        'firstName': user.first_name,
+        'surname': user.surname,
+        'partnerCode': user.partner_code
+    })
+
 
 @auth.verify_token
 def verify_token(token):
