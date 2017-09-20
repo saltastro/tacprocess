8782e5d5ce3a8123d19cd25c673d1df2f7f8bf47
nhlavutelo <nhlavutelo@saao.ac.za>
2017-09-20 09:57:51 +0200

    this commit goes with last commit

---
+++ server/data/proposals.py
@@ -0,0 +1,18 @@
+from data.my_mapper import Base
+
+
+ProposalModel = Base.classes.Proposal
+ProposalCodeModel = Base.classes.ProposalCode
+ProposalContactModel = Base.classes.ProposalContact
+InvestigatorModel = Base.classes.Investigator
+SemesterModel = Base.classes.Semester
+SemesterPhaseModel = Base.classes.SemesterPhase
+MultiPartnerModel = Base.classes.MultiPartner
+ProposalTypeModel = Base.classes.ProposalType
+PartnerModel = Base.classes.Partner
+ProposalStatusModel = Base.classes.ProposalStatus
+P1ObservingConditionsModel = Base.classes.P1ObservingConditions
+TransparencyModel = Base.classes.Transparency
+
+
+
