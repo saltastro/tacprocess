177f441522567b86f63fcb297779a1d49ee2e1eb
nhlavutelo <nhlavutelo@saao.ac.za>
2017-10-03 10:10:28 +0200

    Authorised routes

---
+++ client/salt-tac-pages/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { Route }  from "react-router-dom";
+
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import StatisticsPage from "./components/pages/StatisticsPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
+
+
+
+const App = ({ location }) => (
+  <div className="ui container">
+    <Route location={location} path="/" exact component={HomePage} />
+    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <UserRoute location={location} path="/statistics" exact component={StatisticsPage} />
+  </div>
+);
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+}
+
+export default App;
