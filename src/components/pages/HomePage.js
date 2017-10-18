df4ec901be4018c31b9b9e5f204d308ea32e05b2
nhlavutelo <nhlavutelo@saao.ac.za>
2017-10-18 15:13:33 +0200

    semantic ui setu and some dummy pages

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div>
+    <h1>Home page</h1>
+    <Link to="/login"> login </Link>
+  </div>
+  )
+
+export default HomePage;
