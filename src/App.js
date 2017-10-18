4d894801a2126279a39f5dd702e804c3f70b2140
nhlavutelo <nhlavutelo@saao.ac.za>
2017-10-18 14:45:44 +0200

    Configure eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Now I am running</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div><h1>I am working</h1></div>
+  )
 
 export default App;
