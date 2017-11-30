6cb7c9e23253c66119579688bdf7bf8e238153d1
Christian Hettlage <hettlage@saao.ac.za>
2017-11-30 11:12:05 +0200

    Fixed transparency histogram

--- src/components/plots/TransparencyDistribution.js
+++ src/components/plots/TransparencyDistribution.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { observingTimeForTransparency } from '../../util';
 import Histogram from './Histogram';
 
 /**
  *
  */
 export default class TransparencyDistribution extends React.Component {
-    observingTime = (transparency, limitToPartner) => {
-        const proposals = this.props.proposals.slice(0, 10);
-        return proposals
-                .filter(p => p.transparency === transparency) // transparency is correct
-                .reduce((allTimeRequests, p) => [...allTimeRequests, ...p.requestedTime], []) // collect time requests
-                .filter(r => !limitToPartner || r.partner === this.props.partner) // partner is correct
-                .filter(r => r.forSemester === this.props.semester) // semester is correct
-                .reduce((sum, request) => sum + request.time, 0); // add up all time requests
-    };
-
     render() {
-        const proposals = this.props.proposals.slice(0, 10);
         const transparencies = ['Clear', 'Thin cloud', 'Thick Cloud', 'Any'];
 
         // generate an object of transparencies and observing times
-        const observingTimes = (limitToPartner) => {
+        const observingTimes = (partner) => {
             return transparencies
-                    .reduce((prev, t) =>
+                    .reduce((prev, transparency) =>
                             {
                                 return {
                                     ...prev,
-                                    [t]: this.observingTime(t, false)
+                                    [transparency]: observingTimeForTransparency(this.props.proposals,
+                                                                                 this.props.semester,
+                                                                                 transparency,
+                                                                                 partner)
                                 };
                             }, {});
         };
