b9bbeb2bda88877b4ed1f3d6de6d5b0c1c72935b
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-07 17:36:53 +0200

    included lodash

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,27 @@
-import { USER_LOGGED_IN, USER_LOGGED_OUT } from "../types";
+import { USER_LOGGED_IN, USER_LOGGED_OUT, FETCHING_USER, FAIL_TO_GET_USER } from "../types";
 
 export default function user(state = {}, action = {}) {
   switch (action.type) {
     case USER_LOGGED_IN:
-      return action.user;
+      return {
+        ...state,
+        user: action.user
+      };
 
     case USER_LOGGED_OUT:
       return {};
+
+    case FETCHING_USER:
+      return {
+        ...state,
+        user: {}
+      };
+
+    case FAIL_TO_GET_USER:
+      return {
+        ...state,
+        user: {}
+      };
 
     default:
       return state;
