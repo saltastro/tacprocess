9b5df80445d418e2a962257ec4649b213cb7cc70
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-08 12:17:17 +0200

    fixing renaming

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 import user from "./reducers/user";
 import statistics from "./reducers/statistics";
-import selectors from "./reducers/selectors";
+import filters from "./reducers/filters";
 
 export default combineReducers({
   user,
   statistics,
-  selectors
+  filters
 });
