b62a834c24229c54119b5050a0a04282e6a64233
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-08 16:05:49 +0200

    plotting a available time table

--- src/components/pages/TimeAllocationPage.js
+++ src/components/pages/TimeAllocationPage.js
@@ -1,10 +1,26 @@
+/* eslint-disable */
 import React from "react";
+import { connect } from "react-redux"
+import propTypes from "prop-types";
 import InfoMessage from "../messages/InfoMessage";
+import AllocAvailTable from "../tables/AllocAvailTable";
 
-const TimeAllocationPage = () => (
-  <div>
-    <InfoMessage page="Time Allocation"/>
-  </div>
-  )
 
-export default TimeAllocationPage;
+class TimeAllocationPage extends React.Component {
+
+  render() {
+    const { allocatedTime } = this.props
+    return(
+      <div>
+        <InfoMessage page="Time Allocation"/>
+        <AllocAvailTable allocatedTime={allocatedTime} />
+      </div>
+      );
+    }
+  }
+
+  export default connect(
+    store => ({
+      allocatedTime:store.tac.data,
+    }),null
+  )(TimeAllocationPage);
