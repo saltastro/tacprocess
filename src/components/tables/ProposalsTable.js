a5107add828fef79aa06f784045e04c8e0dc0ebd
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-08 17:19:19 +0200

    changing proposal table to this.

--- src/components/tables/ProposalsTable.js
+++ src/components/tables/ProposalsTable.js
@@ -5,24 +5,32 @@ const ProposalsTable = (proposals) => {
 
 
   return (
-    <div>
+    <div className="scroldiv">
     <h1>Proposals Table</h1>
-    <table className="table center-table proposals-table">
+    <table>
       <thead>
         <tr>
-          <th styles="width:100px;" ><h2>Code</h2></th>
+          <th><h2>Code</h2></th>
           <th><h2>Title</h2></th>
           <th><h2>PI</h2></th>
-          <th><h2>Abstract</h2></th>
-          <th><h2>Total Time</h2></th>
-          <th><h2>Min Time</h2></th>
+          <th><h2>Semester</h2></th>
+          <th><h2>TAC comment</h2></th>
+          <th><h2>Total Requested Time</h2></th>
+          <th><h2>P0</h2></th>
+          <th><h2>P1</h2></th>
+          <th><h2>P2</h2></th>
+          <th><h2>P3</h2></th>
+          <th><h2>Total P0-P3</h2></th>
+          <th><h2>P4</h2></th>
+          <th><h2>Act on Alert</h2></th>
           <th><h2>Transparency</h2></th>
           <th><h2>Max seeing</h2></th>
+          <th><h2>Hover Info</h2></th>
           <th><h2>Tech Report</h2></th>
         </tr>
       </thead>
       <tbody>
-        {
+         { /*
           proposals.proposals.map(p => (
               <tr key={ p.proposalId }>
                 <td  >{ p.proposalCode }</td>
@@ -36,7 +44,40 @@ const ProposalsTable = (proposals) => {
                 <td>{ p.report }</td>
               </tr>
             ))
-        }
+        / */ }
+        <tr>
+          <td>code</td>
+          <td>title</td>
+          <td>prince Invest</td>
+          <td>2017-1</td>
+          <td>
+              <textarea rows="4" cols="50">
+                this is a test text. this field can be empty
+              </textarea>
+          </td>
+          <td>{ 0 }</td>
+          <td>
+              <input type="number" name="p0" value="0" />
+          </td>
+          <td>
+              <input type="number" name="p1" value="0" />
+          </td>
+          <td>
+              <input type="number" name="p2" value="0" />
+          </td>
+          <td>
+              <input type="number" name="p3" value="0" />
+          </td>
+          <td>{ 0 }</td>
+          <td>
+              <input type="number" name="p4" value="0" />
+          </td>
+          <td>false</td>
+          <td>{ 0 }</td>
+          <td>{ 0 }</td>
+          <td>Hover Info</td>
+          <td>Tech Report</td>
+        </tr>
 
       </tbody>
       </table>
