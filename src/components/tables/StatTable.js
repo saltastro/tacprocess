caae0c98e04674ee5cdb1baf79edb196541f1d6c
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-08 16:04:56 +0200

    adding partner table to statistics

--- src/components/tables/StatTable.js
+++ src/components/tables/StatTable.js
@@ -5,15 +5,22 @@ import ProposalTable from "./ProposalTable";
 import ObservingStatTable from "./ObservingStatTable";
 import ConfigStats from "./ConfigStats";
 import ProposalsTable from "./ProposalsTable";
+import PartnerTable from "./PartnerTable";
 
 class StatPropos extends React.Component {
   state = {};
 
   render() {
-    const { proposals} = this.props
+    const { proposals, semester, allocatedTime, partner} = this.props
     return(
       <div>
         <ProposalTable proposals={proposals} />
+        <PartnerTable
+            proposals={proposals}
+            semester={semester}
+            partner={partner}
+            allocatedTime={allocatedTime}
+         />
         <ObservingStatTable proposals={proposals} />
         <ConfigStats proposals={proposals} />
         <ProposalsTable proposals={proposals} />
@@ -24,6 +31,9 @@ class StatPropos extends React.Component {
 
   StatPropos.propTypes = {
     proposals: propTypes.array.isRequired,
+    allocatedTime: propTypes.object.isRequired,
+    semester: propTypes.string.isRequired,
+    partner: propTypes.string.isRequired,
   }
 
 export default StatPropos;
