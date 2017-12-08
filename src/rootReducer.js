e1d3cebe97c4ae270fa13a130212b856143cdf31
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-08 13:57:18 +0200

    adding tac types and tac to root reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import user from "./reducers/user";
 import statistics from "./reducers/statistics";
 import filters from "./reducers/filters";
+import tac from "./reducers/tac";
 
 export default combineReducers({
   user,
   statistics,
-  filters
+  filters,
+  tac
 });
