dab13806a15847067681c020f6d1612145c0cb23
nhlavutelo <nhlavutelo@saao.ac.za>
2017-12-18 13:34:07 +0200

    initial state of a user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,17 @@
 import { USER_LOGGED_IN, USER_LOGGED_OUT, FETCHING_USER, FAIL_TO_GET_USER } from "../types";
 
-export default function user(state = {}, action = {}) {
+
+const initialState = {
+  user: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    partners:[]
+}}
+
+
+export default function user(state = initialState, action = {}) {
   switch (action.type) {
     case USER_LOGGED_IN:
       return {
