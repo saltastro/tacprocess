768604ed925d1cdbfdd42b6c44f1c806a5002f54
Eb-Zeero <macebelene@gmail.com>
2018-01-11 15:30:31 +0200

    considering partner

--- src/util/allocation.js
+++ src/util/allocation.js
@@ -1,24 +1,24 @@
 import { isFloat } from "../util";
 
 
-export function illegalAllocation(proposal, priority) {
-  const t = proposal.allocatedTime[priority];
+export function illegalAllocation(proposal, priority, partner) {
+  const t = proposal.allocatedTime[partner][priority];
   return !isFloat(t) || parseFloat(t) < 0;
 }
 
-export function checkAllocatedTimes(proposals){
+export function checkAllocatedTimes(proposals, partner){
   /*
    *
    *
    * @param proposals and array of proposals
    * @return true if all the allocated times correct else false
    */
-  return !proposals.some(illegalAllocation)
+  return !proposals.some( p => illegalAllocation(p, "p0", partner))
 }
 
 export function getQuaryToAddAllocation(proposals, partner, semester){
   const pList = []
-  if (checkAllocatedTimes(proposals)){
+  if (checkAllocatedTimes(proposals, partner)){
 
     proposals.forEach(p => (
       [0, 1, 2, 3, 4].forEach( t => {
