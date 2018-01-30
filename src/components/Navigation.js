9055e2772354b99786bcc3e0c38330b5cf667844
Eb-Zeero <macebelene@gmail.com>
2018-01-30 09:41:02 +0200

    Navigation bar show pages to who the user is Refactoring some code: * putting a semicolon. * tab now is 4 spaces

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { connect } from "react-redux";
 import * as actions from "../actions/auth";
 import { fetchStatData } from "../actions/statisticsActions";
@@ -7,76 +7,77 @@ import  fetchProposals  from "../actions/proposalsActions";
 import  fetchTargets  from "../actions/targetsActions";
 import { storePartnerAllocations  } from "../actions/timeAllocationActions";
 import {
-    HOME_PAGE,
-    STATISTICS_PAGE,
-    DOCUMENTATION_PAGE,
-    TECHNICAL_PAGE,
-    TAC_PAGE,
-    ADMIN_PAGE
- } from "../types"
+	HOME_PAGE,
+	STATISTICS_PAGE,
+	DOCUMENTATION_PAGE,
+	TECHNICAL_PAGE,
+	TAC_PAGE,
+	ADMIN_PAGE, TAC_CHAIR, TAC_MEMBER, SALT_ASTRONOMER, ADMINISTRATOR
+} from "../types"
+import {canViewPage} from "../util";
 
 class Navigation extends React.Component {
-
-  componentDidMount() {
-    const selected = this.props.filters
-    const {dispatch } = this.props
-
-    dispatch(actions.fetchUserData())
-
-    dispatch(fetchStatData(
-      selected.selectedSemester,
-      selected.selectedPartner
-    ))
-    dispatch(
-      fetchTargets(
-      selected.selectedSemester,
-      selected.selectedPartner
-    ))
-    dispatch(
-      fetchProposals(
-      selected.selectedSemester,
-      selected.selectedPartner
-    ))
-
-    dispatch(storePartnerAllocations(
-      selected.selectedSemester,
-      selected.selectedPartner
-    ))
-  }
-
-  loggingOut() {
-    const { dispatch } = this.props
-    dispatch(actions.logout())
-  }
-
-  render() {
-    const { filters  } = this.props
-
-    return(
-      <div>
-        <ul className="nav">
-          <li className={filters.currentPage === HOME_PAGE ? "active" : ""} ><Link to="/">HOME</Link></li>
-          <li className={filters.currentPage === TECHNICAL_PAGE ? "active" : ""} ><Link to="/techreview">TECH REVIEW</Link></li>
-          <li className={filters.currentPage === STATISTICS_PAGE ? "active" : ""} ><Link to="/statistics">STATISTICS</Link></li>
-          <li className={filters.currentPage === TAC_PAGE ? "active" : ""} ><Link to="/timeallocation">TIME ALLOCATION</Link></li>
-          <li className={filters.currentPage === DOCUMENTATION_PAGE ? "active" : ""} ><Link to="/documentation">DOCUMENTATION</Link></li>
-          <li className={filters.currentPage === ADMIN_PAGE ? "active" : ""}><Link to="/admin">ADMIN</Link></li>
-          <button className="logoutbtn"
-          onClick={ this.loggingOut.bind(this) }> Logout</button>
-        </ul>
-        <ul className="bigNav">
-           <h1>{ filters.currentPage }</h1>
-        </ul>
-      </div>
-      );
-    }
-  }
+	
+	componentDidMount() {
+		const selected = this.props.filters;
+		const {dispatch } = this.props;
+		
+		dispatch(actions.fetchUserData());
+		
+		dispatch(fetchStatData(
+			selected.selectedSemester,
+			selected.selectedPartner
+		));
+		dispatch(
+			fetchTargets(
+				selected.selectedSemester,
+				selected.selectedPartner
+			));
+		dispatch(
+			fetchProposals(
+				selected.selectedSemester,
+				selected.selectedPartner
+			));
+		
+		dispatch(storePartnerAllocations(
+			selected.selectedSemester,
+			selected.selectedPartner
+		))
+	}
+	
+	loggingOut() {
+		const { dispatch } = this.props;
+		dispatch(actions.logout())
+	}
+	
+	render() {
+		const { filters, userRoles  } = this.props;
+		
+		return(
+			<div>
+				<ul className="nav">
+					<li><NavLink to="/">HOME</NavLink></li>
+					{ canViewPage(userRoles, TECHNICAL_PAGE) ? <li><NavLink to="/techreview">TECH REVIEW</NavLink></li> : <li /> }
+					{ canViewPage(userRoles, STATISTICS_PAGE) ? <li><NavLink to="/statistics">STATISTICS</NavLink></li> : <li /> }
+					{ canViewPage(userRoles, TAC_PAGE) ? <li><NavLink to="/timeallocation">TIME ALLOCATION</NavLink></li> : <li /> }
+					{ canViewPage(userRoles, DOCUMENTATION_PAGE) ? <li><NavLink to="/documentation">DOCUMENTATION</NavLink></li> : <li /> }
+					{ canViewPage(userRoles, ADMIN_PAGE) ? <li><NavLink to="/admin">ADMIN</NavLink></li> : <li /> }
+					<button className="logoutbtn"
+					        onClick={ this.loggingOut.bind(this) }> Logout</button>
+				</ul>
+				<ul className="bigNav">
+					<h1>{ filters.currentPage }</h1>
+				</ul>
+			</div>
+		);
+	}
+}
 
 export default connect(
-  store => ({
-    filters: store.filters,
-    statistics:store.statistics,
-    user:store.user.user,
-    SALTAstronomers: store.SALTAstronomers.SALTAstronomer
-   }), null
+	store => ({
+		filters: store.filters,
+		statistics:store.statistics,
+		userRoles:store.user.user.roles,
+		SALTAstronomers: store.SALTAstronomers.SALTAstronomer
+	}), null
 )(Navigation);
--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,6 @@
 import * as types from '../types';
+import {TAC_CHAIR, TAC_MEMBER, TAC_PAGE, TECHNICAL_PAGE, SALT_ASTRONOMER, STATISTICS_PAGE, DOCUMENTATION_PAGE} from "../types";
+import {ADMINISTRATOR} from "../types";
 
 /**
  * Get the observing time for a semester in a proposal.
@@ -10,11 +12,11 @@ import * as types from '../types';
  * @returns {*}
  */
 export function proposalObservingTime(proposal, semester, partner='All') {
-    return proposal.timeRequests
-            .filter(r => r.semester === semester) // semester is correct
-            .reduce((allDistributionItems, r) => [...allDistributionItems, ...r.distribution], []) // collect all partner time requests
-            .filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
-            .reduce((sum, d) => sum + d.time, 0); // add up all time requests
+	return proposal.timeRequests
+	.filter(r => r.semester === semester) // semester is correct
+	.reduce((allDistributionItems, r) => [...allDistributionItems, ...r.distribution], []) // collect all partner time requests
+	.filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
+	.reduce((sum, d) => sum + d.time, 0); // add up all time requests
 }
 /**
  * Get the observing time requested for a semester and transparency in a list of proposals.
@@ -26,13 +28,13 @@ export function proposalObservingTime(proposal, semester, partner='All') {
  * @param partner
  */
 export function observingTimeForTransparency(proposals, semester, transparency, partner='All') {
-    return proposals
-            .filter(p => p.transparency === transparency) // transparency is correct
-            .reduce((allTimeRequests, p) => [...allTimeRequests, ...p.timeRequests], []) // collect time requests
-            .filter(r => r.semester === semester) // semester is correct
-            .reduce((allDistributionItems, r) => [...allDistributionItems, ...r.distribution], []) // collect partner time requests
-            .filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
-            .reduce((sum, request) => sum + request.time, 0); // add up all time requests
+	return proposals
+	.filter(p => p.transparency === transparency) // transparency is correct
+	.reduce((allTimeRequests, p) => [...allTimeRequests, ...p.timeRequests], []) // collect time requests
+	.filter(r => r.semester === semester) // semester is correct
+	.reduce((allDistributionItems, r) => [...allDistributionItems, ...r.distribution], []) // collect partner time requests
+	.filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
+	.reduce((sum, request) => sum + request.time, 0); // add up all time requests
 }
 
 /**
@@ -48,13 +50,13 @@ export function observingTimeForTransparency(proposals, semester, transparency, 
  * @param partner
  */
 export function observingTimeForSeeing(proposals, semester, seeingRange, partner='All') {
-    return proposals
-            .filter(p => seeingRange[0] <= p.maxSeeing && p.maxSeeing < seeingRange[1])// seeing is correct
-            .reduce((allTimeRequests, p) => [...allTimeRequests, ...p.timeRequests], []) // collect time requests
-            .filter(r => r.semester === semester) // semester is correct
-            .reduce((allDistributionItems, r) => [...allDistributionItems, ...r.distribution], []) // collect partner time requests
-            .filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
-            .reduce((sum, request) => sum + request.time, 0); // add up all time requests
+	return proposals
+	.filter(p => seeingRange[0] <= p.maxSeeing && p.maxSeeing < seeingRange[1])// seeing is correct
+	.reduce((allTimeRequests, p) => [...allTimeRequests, ...p.timeRequests], []) // collect time requests
+	.filter(r => r.semester === semester) // semester is correct
+	.reduce((allDistributionItems, r) => [...allDistributionItems, ...r.distribution], []) // collect partner time requests
+	.filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
+	.reduce((sum, request) => sum + request.time, 0); // add up all time requests
 }
 
 /**
@@ -64,8 +66,8 @@ export function observingTimeForSeeing(proposals, semester, seeingRange, partner
  * @returns {*}
  */
 export function instrumentCount(proposal) {
-    return Object.keys(proposal.instruments)
-            .reduce((sum, key) => sum + proposal.instruments[key].length, 0);
+	return Object.keys(proposal.instruments)
+	.reduce((sum, key) => sum + proposal.instruments[key].length, 0);
 }
 
 /**
@@ -83,7 +85,7 @@ export function instrumentCount(proposal) {
  *
  * observingTimeForInstrument(proposals, '2017-1', 'rss', {partner: 'RSA'})
  * observingTimeForInstrument(proposals, '2017-1', 'rss', {partner: 'RSA', field: 'mode', value: 'Polarimetry'})
-
+ 
  * @param proposal
  * @param semester
  * @param instrument
@@ -92,19 +94,19 @@ export function instrumentCount(proposal) {
  * @param partner
  */
 export function proposalObservingTimeForInstrument(proposal, semester, instrument, {field, value, partner='All'}) {
-    const instrumentModeCount = (proposal.instruments[instrument.toLowerCase()] || [])
-            .filter(v => !field || v[field] === value)
-            .length;
-
-    const instrumentModeFraction = instrumentModeCount / instrumentCount(proposal);
-
-    const totalObservingTime = proposal.timeRequests
-            .filter(r => r.semester === semester) // semester is correct
-            .reduce((distributionItems, r) => [...distributionItems, ...r.distribution], []) // collect all partner time requests
-            .filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
-            .reduce((sum, d) => sum + d.time, 0); // add all time requests
-
-    return instrumentModeFraction * totalObservingTime;
+	const instrumentModeCount = (proposal.instruments[instrument.toLowerCase()] || [])
+	.filter(v => !field || v[field] === value)
+		.length;
+	
+	const instrumentModeFraction = instrumentModeCount / instrumentCount(proposal);
+	
+	const totalObservingTime = proposal.timeRequests
+	.filter(r => r.semester === semester) // semester is correct
+	.reduce((distributionItems, r) => [...distributionItems, ...r.distribution], []) // collect all partner time requests
+	.filter(d => partner === 'All' || d.partnerCode === partner) // partner is correct
+	.reduce((sum, d) => sum + d.time, 0); // add all time requests
+	
+	return instrumentModeFraction * totalObservingTime;
 }
 
 /**
@@ -122,7 +124,7 @@ export function proposalObservingTimeForInstrument(proposal, semester, instrumen
  *
  * observingTimeForInstrument(proposals, '2017-1', 'rss', {partner: 'RSA'})
  * observingTimeForInstrument(proposals, '2017-1', 'rss', {partner: 'RSA', field: 'mode', value: 'Polarimetry'})
-
+ 
  * @param proposals
  * @param semester
  * @param instrument
@@ -131,12 +133,12 @@ export function proposalObservingTimeForInstrument(proposal, semester, instrumen
  * @param partner
  */
 export function observingTimeForInstrument(proposals, semester, instrument, {field, value, partner}) {
-    return proposals
-            .reduce((sum, proposal) =>
-                            sum + proposalObservingTimeForInstrument(proposal,
-                                                                     semester,
-                                                                     instrument,
-                                                                     {field, value, partner}), 0);
+	return proposals
+	.reduce((sum, proposal) =>
+		sum + proposalObservingTimeForInstrument(proposal,
+		semester,
+		instrument,
+		{field, value, partner}), 0);
 }
 
 /**
@@ -148,15 +150,15 @@ export function observingTimeForInstrument(proposals, semester, instrument, {fie
  * @param user
  */
 export function partners(user) {
-    if (!user.roles) {
-        return [];
-    }
-    const initial = hasRole(user, types.ADMINISTRATOR) || hasRole(user, types.SALT_ASTRONOMER) ? [types.ALL_PARTNER] : [];
-    const rolePartners = user.roles // collect partners from all roles
-            .reduce((prev, role) => [...(role.partners || []), ...prev], initial);
-    const partnerSet = new Set(rolePartners);
-
-    return Array.from(partnerSet).sort();
+	if (!user.roles) {
+		return [];
+	}
+	const initial = hasRole(user, types.ADMINISTRATOR) || hasRole(user, types.SALT_ASTRONOMER) ? [types.ALL_PARTNER] : [];
+	const rolePartners = user.roles // collect partners from all roles
+	.reduce((prev, role) => [...(role.partners || []), ...prev], initial);
+	const partnerSet = new Set(rolePartners);
+	
+	return Array.from(partnerSet).sort();
 }
 
 /**
@@ -171,158 +173,172 @@ export function partners(user) {
  * @param partner
  */
 export function hasRole(user, role, partner) {
-    if (role === types.ADMINISTRATOR || role === types.SALT_ASTRONOMER) {
-        return (user.roles || []).some(r => r.type === role);
-    } else {
-        return (user.roles || []).some(r => r.type === role && (r.partners || []).includes(partner));
-    }
+	if (role === types.ADMINISTRATOR || role === types.SALT_ASTRONOMER) {
+		return (user.roles || []).some(r => r.type === role);
+	} else {
+		return (user.roles || []).some(r => r.type === role && (r.partners || []).includes(partner));
+	}
 }
 
 export function canDo(user, action, partner, proposal) {
-    switch (action) {
-    case types.VIEW_TIME_ALLOCATION_PAGE:
-        return hasRole(user, types.TAC_MEMBER, partner) ||
-                hasRole(user, types.SALT_ASTRONOMER) ||
-                hasRole(user, types.ADMINISTRATOR);
-    case types.EDIT_TIME_ALLOCATION_PAGE:
-        return hasRole(user, types.TAC_CHAIR, partner) ||
-                hasRole(user, types.ADMINISTRATOR);
-    case types.CHANGE_LIAISON:
-        return hasRole(user, types.ADMINISTRATOR);
-    case types.SELF_ASSIGN_TO_PROPOSAL:
-        return hasRole(user, types.SALT_ASTRONOMER);
-    default:
-        return false;
-    }
+	switch (action) {
+		case types.VIEW_TIME_ALLOCATION_PAGE:
+			return hasRole(user, types.TAC_MEMBER, partner) ||
+				hasRole(user, types.SALT_ASTRONOMER) ||
+				hasRole(user, types.ADMINISTRATOR);
+		case types.EDIT_TIME_ALLOCATION_PAGE:
+			return hasRole(user, types.TAC_CHAIR, partner) ||
+				hasRole(user, types.ADMINISTRATOR);
+		case types.CHANGE_LIAISON:
+			return hasRole(user, types.ADMINISTRATOR);
+		case types.SELF_ASSIGN_TO_PROPOSAL:
+			return hasRole(user, types.SALT_ASTRONOMER);
+		default:
+			return false;
+	}
 }
 
 // This utility method checks if a proposal has assigned an Astronomer or not
 export const astronomerAssigned = (proposal) => {
-    if(!proposal.SALTAstronomer){
-      return true;
-    }
-}
+	if(!proposal.SALTAstronomer){
+		return true;
+	}
+};
 
 export function isFloat(val) {
-    const floatRegex = /^[+-]?\d+(?:[.,]\d*?)?$/;
-    if (!floatRegex.test(val))
-        return false;
-
-    const temp = parseFloat(val);
-    if (isNaN(temp))
-        return false;
-    return true;
+	const floatRegex = /^[+-]?\d+(?:[.,]\d*?)?$/;
+	if (!floatRegex.test(val))
+		return false;
+	
+	const temp = parseFloat(val);
+	if (isNaN(temp))
+		return false;
+	return true;
 }
 
 export function canUserWriteAllocations(user, partner){
-  let canWrite = false;
-  user.roles.forEach( r => {
-            if ((r.type === "ADMINISTRATOR" || r.type === "TAC_CHAIR") &&
-                    r.partners.some(p => (p === partner))){
-                canWrite = true;
-              }
-            }
-  );
-  return canWrite;
+	let canWrite = false;
+	user.roles.forEach( r => {
+			if ((r.type === "ADMINISTRATOR" || r.type === "TAC_CHAIR") &&
+				r.partners.some(p => (p === partner))){
+				canWrite = true;
+			}
+		}
+	);
+	return canWrite;
 }
 
 export function canUserWriteTechComments(user, partner){
-  let canWrite = false;
-
-
-  user.roles.forEach( r => {
-            if ((r.type === "ADMINISTRATOR" || r.type === "SALT_ASTRONOMER") &&
-                    r.partners.some(p => (p === partner))){
-                canWrite = true;
-              }
-            }
-  );
-  return canWrite;
+	let canWrite = false;
+	
+	
+	user.roles.forEach( r => {
+			if ((r.type === "ADMINISTRATOR" || r.type === "SALT_ASTRONOMER") &&
+				r.partners.some(p => (p === partner))){
+				canWrite = true;
+			}
+		}
+	);
+	return canWrite;
 }
 
 export function allocatedTimeTotals( proposals, partner ){
-  /**
-   *
-   *
-   * @param partner
-   * @param availableTime
-   * @param proposals
-   * @return object of allocated time totals per priority
-   */
-
-   let total = {
-     p0: 0,
-     p1: 0,
-     p2: 0,
-     p3: 0,
-     p4: 0
-   }
-   proposals.forEach(p => {
-     [0, 1, 2, 3, 4].forEach( pr => {
-       total[`p${pr}`] += parseFloat(p.allocatedTime[partner][`p${pr}`]) || 0
-     })
-   })
-   return total
-
+	/**
+	 *
+	 *
+	 * @param partner
+	 * @param availableTime
+	 * @param proposals
+	 * @return object of allocated time totals per priority
+	 */
+	
+	let total = {
+		p0: 0,
+		p1: 0,
+		p2: 0,
+		p3: 0,
+		p4: 0
+	};
+	proposals.forEach(p => {
+		[0, 1, 2, 3, 4].forEach( pr => {
+			total[`p${pr}`] += parseFloat(p.allocatedTime[partner][`p${pr}`]) || 0
+		})
+	});
+	return total
+	
 }
 
 export function areAllocatedTimesCorrect(partner, availableTime, proposals){
-  /**
-   *
-   *
-   * @param partner
-   * @param availableTime
-   * @param proposals
-   * @return object stating if allocated time of proposals doen't exceed available time and charector are numbers
-   */
-   const allocTotals = allocatedTimeTotals( proposals, partner )
-
-   return {
-     p0p1: allocTotals.p0 + allocTotals.p1 <= availableTime.p0p1 *60*60,
-     p2: allocTotals.p2 <= availableTime.p2*60*60,
-     p3: allocTotals.p3 <= availableTime.p3*60*60,
-   }
-
+	/**
+	 *
+	 *
+	 * @param partner
+	 * @param availableTime
+	 * @param proposals
+	 * @return object stating if allocated time of proposals doen't exceed available time and charector are numbers
+	 */
+	const allocTotals = allocatedTimeTotals( proposals, partner );
+	
+	return {
+		p0p1: allocTotals.p0 + allocTotals.p1 <= availableTime.p0p1 *60*60,
+		p2: allocTotals.p2 <= availableTime.p2*60*60,
+		p3: allocTotals.p3 <= availableTime.p3*60*60,
+	}
+	
 }
 
 export function updateLiaisonAstronomerForProposal (proposals, proposalToUpdate, liaisonUsername){
-  const updated = (proposals || []).map(p => {
-
-    if ( p.proposalCode === proposalToUpdate ){
-       p.liaisonAstronomer = liaisonUsername
-    }
-    return p
-  })
-  return updated
+	const updated = (proposals || []).map(p => {
+		
+		if ( p.proposalCode === proposalToUpdate ){
+			p.liaisonAstronomer = liaisonUsername
+		}
+		return p
+	});
+	return updated
 }
 
 export function updateTechnicalCommentForProposal(proposals, proposalToUpdate, techReport){
-  const updated = (proposals || []).map(p => {
-
-    if ( p.proposalCode === proposalToUpdate ){
-       p.techReport = techReport
-    }
-    return p
-  })
-  return updated
+	const updated = (proposals || []).map(p => {
+		
+		if ( p.proposalCode === proposalToUpdate ){
+			p.techReport = techReport
+		}
+		return p
+	});
+	return updated
 }
 
 export function getLiaisonName(username, SALTAstronomers){
-  let name
-  (SALTAstronomers || []).forEach( sa => {
-    if (sa.username === username){
-      name = sa.name
-    }
-  })
-  return name
+	let name;
+	(SALTAstronomers || []).forEach( sa => {
+		if (sa.username === username){
+			name = sa.name
+		}
+	});
+	return name
 }
 
 export function getLiaisonUsername(name, SALTAstronomers){
-  let username
-  (SALTAstronomers || []).forEach( sa => {
-    if (sa.name === name){
-      username = sa.username
-    }
-  })
-  return username
+	let username;
+	(SALTAstronomers || []).forEach( sa => {
+		if (sa.name === name){
+			username = sa.username
+		}
+	});
+	return username
+}
+
+const pageRole = (page, role) => {
+	if (page === TAC_PAGE && (role.toLowerCase() === TAC_CHAIR.toLowerCase() || role.toLowerCase() === TAC_MEMBER.toLowerCase())) { return true }
+	if (page === TECHNICAL_PAGE && (role.toLowerCase() === SALT_ASTRONOMER.toLowerCase() )) { return true }
+	return page === STATISTICS_PAGE || page === DOCUMENTATION_PAGE;
+	
+};
+
+export function canViewPage (userRoles, page){
+	if ((userRoles || []).some( p => p.type.toLowerCase() === ADMINISTRATOR.toLowerCase())) {
+		return true;
+	}
+	return (userRoles || []).some( p => pageRole(page, p.type))
 }
