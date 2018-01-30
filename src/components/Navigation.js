48dbc9edd6b642c072e8edccaa25eebdeed34ef9
Eb-Zeero <macebelene@gmail.com>
2018-01-30 10:09:23 +0200

    will fix navigation bar in the future it has warning

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom" // TODO: use NavLink and make sure that NavLink work
 import { connect } from "react-redux";
 import * as actions from "../actions/auth";
 import { fetchStatData } from "../actions/statisticsActions";
@@ -7,12 +7,11 @@ import  fetchProposals  from "../actions/proposalsActions";
 import  fetchTargets  from "../actions/targetsActions";
 import { storePartnerAllocations  } from "../actions/timeAllocationActions";
 import {
-	HOME_PAGE,
 	STATISTICS_PAGE,
 	DOCUMENTATION_PAGE,
 	TECHNICAL_PAGE,
 	TAC_PAGE,
-	ADMIN_PAGE, TAC_CHAIR, TAC_MEMBER, SALT_ASTRONOMER, ADMINISTRATOR
+	ADMIN_PAGE
 } from "../types"
 import {canViewPage} from "../util";
 
@@ -51,22 +50,32 @@ class Navigation extends React.Component {
 	}
 	
 	render() {
-		const { filters, userRoles  } = this.props;
+		const { currentPage, userRoles  } = this.props;
 		
 		return(
 			<div>
 				<ul className="nav">
-					<li><NavLink to="/">HOME</NavLink></li>
-					{ canViewPage(userRoles, TECHNICAL_PAGE) ? <li><NavLink to="/techreview">TECH REVIEW</NavLink></li> : <li /> }
-					{ canViewPage(userRoles, STATISTICS_PAGE) ? <li><NavLink to="/statistics">STATISTICS</NavLink></li> : <li /> }
-					{ canViewPage(userRoles, TAC_PAGE) ? <li><NavLink to="/timeallocation">TIME ALLOCATION</NavLink></li> : <li /> }
-					{ canViewPage(userRoles, DOCUMENTATION_PAGE) ? <li><NavLink to="/documentation">DOCUMENTATION</NavLink></li> : <li /> }
-					{ canViewPage(userRoles, ADMIN_PAGE) ? <li><NavLink to="/admin">ADMIN</NavLink></li> : <li /> }
+					<li><Link to="/">HOME</Link></li>
+					{ canViewPage(userRoles, TECHNICAL_PAGE) ?
+						<li className={currentPage === TECHNICAL_PAGE ? "active" : ""}>
+							<Link to="/techreview">TECH REVIEW</Link></li> : <li /> }
+					{ canViewPage(userRoles, STATISTICS_PAGE) ?
+						<li className={currentPage === STATISTICS_PAGE ? "active" : ""}>
+							<Link to="/statistics">STATISTICS</Link></li> : <li /> }
+					{ canViewPage(userRoles, TAC_PAGE) ?
+						<li className={currentPage === TAC_PAGE ? "active" : ""}>
+							<Link to="/timeallocation">TIME ALLOCATION</Link></li> : <li /> }
+					{ canViewPage(userRoles, DOCUMENTATION_PAGE) ?
+						<li className={currentPage === DOCUMENTATION_PAGE ? "active" : ""}>
+							<Link to="/documentation">DOCUMENTATION</Link></li> : <li /> }
+					{ canViewPage(userRoles, ADMIN_PAGE) ?
+						<li className={currentPage === ADMIN_PAGE ? "active" : ""}>
+							<Link to="/admin">ADMIN</Link></li> : <li /> }
 					<button className="logoutbtn"
 					        onClick={ this.loggingOut.bind(this) }> Logout</button>
 				</ul>
 				<ul className="bigNav">
-					<h1>{ filters.currentPage }</h1>
+					<h1>{ currentPage }</h1>
 				</ul>
 			</div>
 		);
@@ -75,6 +84,7 @@ class Navigation extends React.Component {
 
 export default connect(
 	store => ({
+		currentPage: store.filters.currentPage,
 		filters: store.filters,
 		statistics:store.statistics,
 		userRoles:store.user.user.roles,
