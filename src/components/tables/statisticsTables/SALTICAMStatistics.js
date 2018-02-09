df863412fd792d6ce4ab0ceca57dcd96790ec5ec
Eb-Zeero <macebelene@gmail.com>
2018-02-09 16:34:38 +0200

    recreating statistics table in other

---
+++ src/components/tables/statisticsTables/SALTICAMStatistics.js
@@ -0,0 +1,16 @@
+import React from "react";
+import propTypes from "prop-types";
+
+const SALTICAMStatistics = (proposals) => {
+    return(
+        <div>
+        
+        </div>
+    )
+};
+
+SALTICAMStatistics.propTypes = {
+    proposals: propTypes.array.isRequired,
+}
+
+export default SALTICAMStatistics;
