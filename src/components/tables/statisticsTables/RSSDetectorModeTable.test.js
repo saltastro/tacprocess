431b02d1c4d0120ff213edc85d74e6f60448eaeb
chynamyerz <myezasifiso11@gmail.com>
2018-02-21 17:18:55 +0200

    Plots and Tables unit tests rewritten/modified to match the updated version of the TACPage

---
+++ src/components/tables/statisticsTables/RSSDetectorModeTable.test.js
@@ -0,0 +1,63 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { shallow, mount, render } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import RSSDetectorModeTable from "./RSSDetectorModeTable";
+
+const proposals = [
+  {
+    transparency: 'Clear',
+    maxSeeing: 2,
+    instruments: {
+      hrs: [ { exposureMode: "HIGH RESOLUTION" } ],
+      rss: [ { dictatorMode: 'NORMAL', mode: 'Spectroscopy' } ],
+      scam: [ { dictatorMode: "NORMAL" } ],
+      bvit: [{type: "BVIT"}]
+    },
+    timeRequests: [
+      {
+        semester: '2018-1',
+        distribution: [ { partnerCode: 'RSA', time: 150 }, { partnerCode: 'UW', time: 200 }, { partnerCode: 'IUCAA', time: 250 } ]
+      },
+      {
+        semester: '2018-1',
+        distribution: [ { partnerCode: 'RSA', time: 400 }, { partnerCode: 'UW', time: 50 } ]
+      }
+    ]
+  },
+  {
+    transparency: 'Thin cloud',
+    maxSeeing: 2,
+    instruments: {
+      hrs: [ { exposureMode: "HIGH RESOLUTION" } ],
+      rss: [ { dictatorMode: 'NORMAL', mode: 'Spectroscopy' } ],
+      scam: [ { dictatorMode: "NORMAL" } ],
+      bvit: [{type: "BVIT"}]
+    },
+    timeRequests: [
+      {
+        semester: '2017-1',
+        distribution: [ { partnerCode: 'RSA', time: 150 }, { partnerCode: 'UW', time: 200 }, { partnerCode: 'IUCAA', time: 250 } ]
+      },
+      {
+        semester: '2018-1',
+        distribution: [ { partnerCode: 'RSA', time: 400 }, { partnerCode: 'UW', time: 50 } ]
+      }
+    ]
+  }
+];
+
+// Checking if the RSSDetectorModeTable Component renders correctly for different input
+describe("RSSDetectorModeTable Component", () => {
+
+  //Enzyme method testing if it renders correctly with empty values of the proposals, semester, and partner
+  it("Render the RSSDetectorModeTable Component having unpopulated props with no errors", () => {
+    const rendered = mount(<RSSDetectorModeTable proposals={[]}  />);
+    expect(shallowToJson(rendered)).toMatchSnapshot();
+  });
+
+  it("Render the RSSDetectorModeTable Component having populated props with no errors", () => {
+    const rendered = mount(<RSSDetectorModeTable proposals={proposals}  />);
+    expect(shallowToJson(rendered)).toMatchSnapshot();
+  });
+});
