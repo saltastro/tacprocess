2fb1bccae2c1caf54b891e4929ca3a72b14a9014
Eb-Zeero <macebelene@gmail.com>
2018-02-27 12:59:53 +0200

    remove unused declaration

--- src/index.js
+++ src/index.js
@@ -9,52 +9,15 @@ import App from './App';
 import "./styles/index.css";
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from "./rootReducer";
-import TotalTimeDistribution from "./components/plots/TotalTimeDistribution";
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
-const proposals = [{
-    transparency: 'Clear',
-    maxSeeing: 2,
-    timeRequests: [
-        {
-            semester: '2017-1',
-            distribution: [
-                {
-                    partnerCode: 'RSA',
-                    time: 150
-                },
-                {
-                    partnerCode: 'UW',
-                    time: 200
-                },
-            ]
-        },
-        {
-            semester: '2017-2',
-            distribution: [
-                {
-                    partnerCode: 'RSA',
-                    time: 400
-                },
-                {
-                    partnerCode: 'UW',
-                    time: 50
-                },
-            ]
-        }
-    ]
-}];
-const semester = '2017-1';
-const partner = 'UW';
-
 ReactDOM.render(
         <Provider store={store}>
             <App/>
         </Provider>,
-        //<TotalTimeDistribution proposals={proposals} semester={semester} partner={partner}/>,
         document.getElementById('root'));
 
 registerServiceWorker();
