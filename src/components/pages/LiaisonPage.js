a8d6e50568bc8a98a36a4cc09653623cda31bc4c
Eb-Zeero <macebelene@gmail.com>
2018-04-17 17:10:24 +0200

    file download call and dummy button added

--- src/components/pages/LiaisonPage.js
+++ src/components/pages/LiaisonPage.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import LiaisonTable from '../tables/LiaisonTable'
+import { downloadSummary } from "../../util";
 
 class LiaisonPage extends React.Component {
-  requestSummary = (papa) => {
-    console.log(papa)
+  requestSummary = (event, proposalCode) => {
+    event.preventDefault();
+    const { semester } = this.props
+    downloadSummary(proposalCode, semester)
   }
   render() {
-    const {proposals, astronomers, user, } = this.props
+    const {proposals, astronomers, user } = this.props
     return (
       <div>
         <h1> Liaison page</h1>
-        <LiaisonTable proposals={proposals} canAssign={true} selectArray={astronomers} requestSummary={this.requestSummary} />
+        <LiaisonTable proposals={proposals} canAssign={false} username={"this guy"} selectArray={astronomers} requestSummary={this.requestSummary} />
+        <button>Submit</button>
       </div>
     )
   }
@@ -23,6 +27,7 @@ export default connect(
     proposals: store.proposals.proposals,
     astronomers: store.SALTAstronomers.SALTAstronomer,
     initProposals: store.proposals.initProposals,
+    semester: store.filters.selectedSemester,
     user: store.user.user
   }), null
 )(LiaisonPage);
