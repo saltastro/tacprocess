643f4dab76cb785d1d38dc8231063546f0db42b7
Eb-Zeero <macebelene@gmail.com>
2018-04-30 16:00:33 +0200

    all data actions and reducers

---
+++ src/actions/data-actions.js
@@ -0,0 +1,69 @@
+import {ALL_PARTNER, FETCHED_DATA, FETCHING_DATA_FAIL, FETCHING_DATA} from '../types'
+import {convertPartnerAllocations, fetchAllocationsPass} from './timeAllocationActions'
+import {convertTargets, FetchTargetsPass} from './targetsActions'
+import {FetchProposalsPass} from './proposalsActions'
+import {convertSA, FetchSAPass} from './saltAstronomerActions'
+import {
+  convertProposals, convertUserData,
+  queryPartnerAllocations,
+  queryProposals,
+  querySALTAstronomers, querySaltUsers, queryTacMembers,
+  queryTargets,
+  queryUserData
+} from '../api/graphQL'
+import {userLoggedIn, partnersFilter} from './auth'
+import {convertSaltUsers, convertTacMembers, fetchSaltUsersPass, fetchTacMembersPass} from './AdminActions'
+
+export const fetchingAllData = () => ({
+  type: FETCHING_DATA,
+});
+
+export const fetchedAllData = () => ({
+  type: FETCHED_DATA,
+});
+
+export const fetchedAllDataFail = (message) => ({
+  type: FETCHING_DATA_FAIL,
+  payload: {
+    error: message
+  }
+});
+
+/**
+ * this method fetch and dispatch all the data needed on the tac pages
+ * data include:
+ *  proposals, targets, user, salt astronomers, partners allocated times, tac members of all partners
+ *  and all salt users
+ *  @param semester
+ *  @param partner
+ * */
+export function fetchAllData(semester, partner){
+  return async (dispatch) => {
+    dispatch(fetchingAllData())
+    try {
+      const saltAstronomers = querySALTAstronomers()
+      const user = queryUserData()
+      const proposals = queryProposals(semester, partner)
+      const targets = queryTargets(semester, partner)
+      const allocations = queryPartnerAllocations(semester, partner)
+      const tacMembers = queryTacMembers()
+      const saltUsers = querySaltUsers()
+      await Promise.all([saltAstronomers, user, proposals, targets, allocations, tacMembers, saltUsers])
+        .then(data => {
+          dispatch(FetchSAPass(convertSA(data[0].data.data)))
+          dispatch(userLoggedIn(convertUserData(data[1].data.data.user)))
+          dispatch(partnersFilter(ALL_PARTNER))
+          dispatch(FetchProposalsPass(convertProposals(data[2].data.data, semester, partner), semester))
+          dispatch(FetchTargetsPass(convertTargets(data[3].data.data)))
+          dispatch(fetchAllocationsPass(convertPartnerAllocations(data[4].data.data)))
+          dispatch(fetchTacMembersPass(convertTacMembers(data[5].data.data)))
+          dispatch(fetchSaltUsersPass(convertSaltUsers(data[6].data.data)))
+        })
+
+    }catch (e) {
+      dispatch(fetchedAllDataFail(e.message))
+      return
+    }
+    dispatch(fetchedAllData())
+  }
+}
