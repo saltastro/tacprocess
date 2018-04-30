20449e5a61f8c1be8b797775c9d001fab35af59f
Eb-Zeero <macebelene@gmail.com>
2018-04-30 16:01:21 +0200

    Loading and failing to load component

---
+++ src/components/messages/FailToLoad.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FailToLoad = () => (
+  <div>
+    <div className="dim-screen-fail">
+        <div id="emoticons">
+          <div className="emoti" data-emoti="sad"/>
+        </div>
+      <div>
+        <h2>Fail to load data please reload</h2>
+      </div>
+    </div>
+  </div>
+)
+
+export default FailToLoad
